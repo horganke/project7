c0f7bbfb1bc86aa6128490dd2da762968973970b
horganke <horganke@msu.edu>
2015-02-16 12:13:11 -0500

    holiday app

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
 # app.rb
 require 'sinatra'
+require 'holidapi'
 
 class MyWebApp < Sinatra::Base
   get '/' do
-  	erb :"index#{ rand(3) + 1 }"
+    @holidays = HolidApi.get
+    @holidays_current = HolidApi.get(month: (Date.today.strftime('%m')))
+    @holidays_birth = HolidApi.get(year:1993,month:10,country: 'us')
+    erb :index
   end
 end
