3f4dec85946d73f3f25d32755a66124a7847801d
horganke <horganke@msu.edu>
2015-02-23 13:37:05 -0500

    fix holiday app

--- app.rb
+++ app.rb
@@ -6,9 +6,15 @@ class MyWebApp < Sinatra::Base
     erb :form
   end
   get '/holidays' do
-    @holidays = HolidApi.get(day: params['day'], month: params['month'], year: params['year'])
-    puts params
-    puts @holidays
+  	if params['year'] != nil && params['year'] != ""
+  		if params['month'] != nil && params['month'] != ""
+  			if params['day'] != nil && params['day'] != ""
+  				@holidays = HolidApi.get(year: params['year'], month: params['month'], day: params['day']).flatten
+  			else
+  				@holidays = HolidApi.get(year: params['year'], month: params['month']).flatten
+  			end
+  		end
+  	end
     erb :index
   end
 end
